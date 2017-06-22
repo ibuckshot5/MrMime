9042bee1e6f08fd76b9c3c7e0f2fe29562b75b0a
Oliver Heering <heering@riege.com>
2017-06-22 09:03:49 +0200

    Configurable logging

--- mrmime/__init__.py
+++ mrmime/__init__.py
@@ -1,8 +1,9 @@
 # Needed for download_remote_config_version request.
 import json
+import logging
 import os
 
-import logging
+log = logging.getLogger(__name__)
 
 APP_VERSION = 6304
 
@@ -11,21 +12,28 @@ API_VERSION = '0.63.1'
 
 DEFAULT_CONFIG_FILE = 'mrmime_config.json'
 
-log = logging.getLogger(__name__)
 
 _mr_mime_cfg = {
+    # --- localization
     'player_locale': {                  # Default player locale
         'country': 'US',
         'language': 'en',
         'timezone': 'America/Denver'
     },
+    # --- general
     'parallel_logins': True,            # Parallel logins increases number of requests.
+    # --- account login specific
     'login_retries': 3,                 # Number of login retries
     'login_delay': 6,                   # Delay between login retries
     'full_login_flow': True,            # Whether login flow requests should be performed or not
     'download_assets_and_items': True,  # Whether to perform download_asset_digest and download_item_templates requests at all
-    'scan_delay': 10                    # Wait at least this long between 2 GMO requests
+    # --- misc requests
+    'scan_delay': 10,                   # Wait at least this long between 2 GMO requests
+    # --- logging
+    'debug_log': False,                 # If MrMime should output debug logging
+    'log_file': None                    # If given MrMime also logs into this file
 }
+
 
 # ---------------------------------------------------------------------------
 
@@ -44,3 +52,12 @@ def init_mr_mime(user_cfg=None, config_file=DEFAULT_CONFIG_FILE):
     if user_cfg:
         log.info("Applying user configuration.")
         _mr_mime_cfg.update(user_cfg)
+
+    if _mr_mime_cfg['debug_log'] is True:
+        logging.getLogger('mrmime').setLevel(logging.DEBUG)
+
+    if _mr_mime_cfg['log_file']:
+        file_handler = logging.FileHandler(_mr_mime_cfg['log_file'])
+        file_handler.setFormatter(
+            logging.Formatter('%(asctime)s [%(levelname)8s] %(message)s'))
+        logging.getLogger('mrmime').addHandler(file_handler)
